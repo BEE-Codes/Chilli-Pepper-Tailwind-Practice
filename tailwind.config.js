888b81c4e43db32c246219684870ced484c5f700
BEE-Codes <brigemmanuel@gmail.com>
2024-11-18 16:01:25 +0100

    This is Chilli Pepper

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+      bonheur:["Bonheur Royale", "cursive"],
+      }
+    },
+  },
+  plugins: [],
+}
+
